55969345905ac9998adc81ef32450b703ec68ce0
Swapnil Shende <swapnilshende1998@gmail.com>
2024-05-21 18:08:14 +0530

    Initial commit

---
+++ src/context/Theme.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+const ThemeContext = createContext({
+  themeMode: "light",
+  lightTheme: () => {},
+  darkTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+
+export default function useTheme() {
+  return useContext(ThemeContext);
+}
